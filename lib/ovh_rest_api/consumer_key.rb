7fd9cb59dc48a289e0159d20486f61c79fc7fbcc
charlie <charlie.eissen@gmail.com>
2013-12-15 08:53:29 +0100

    added consular key generation and spec test with webmock

--- lib/ovh_rest_api/consumer_key.rb
+++ lib/ovh_rest_api/consumer_key.rb
@@ -1,5 +1,27 @@
 module OvhRestApi
-  class OvhRestApi::ConsumerKey
+  class OvhRestApi::ConsumerKey < OvhRestApi::Base
     
+    def self.generate country, api_key, access_rules = nil
+      access_rules ||= all_access_rules
+      uri = URI.parse "#{api_uri(country)}/auth/credential"
+      request = Net::HTTP::Post.new uri.path, {"X-Ovh-Application" => api_key, "Content-type" => "application/json"}
+      request.body = access_rules.to_json
+      http = Net::HTTP.new uri.host, uri.port
+      http.use_ssl = true
+      response = http.request request
+      JSON.parse response.body
+    end
+    
+    def self.all_access_rules
+      {
+        "accessRules" => [
+          { "method" => "GET", "path" => "/*" },
+          { "method" => "POST", "path" => "/*" },
+          { "method" => "PUT", "path" => "/*" },
+          { "method" => "DELETE", "path" => "/*" }
+        ]
+      }
+    end
+
   end
 end
--- spec/lib/ovh_rest_api/consumer_key_spec.rb
+++ spec/lib/ovh_rest_api/consumer_key_spec.rb
@@ -1,5 +1,21 @@
 require "spec_helper"
 
 describe OvhRestApi::ConsumerKey do
-  
+  context "#generate" do
+    before do
+      @returned_body = '{"validationUrl": "https://eu.api.ovh.com/auth/?credentialToken=iQ1joJE0OmSPlUAoSw1IvAPWDeaD87ZM64HEDvYq77IKIxr4bIu6fU8OtrPQEeRh","consumerKey": "MtSwSrPpNjqfVSmJhLbPyr2i45lSwPU1","state": "pendingValidation"}'
+      stub_request(:post, "https://eu.api.ovh.com/1.0/auth/credential").with(headers: {"X-Ovh-Application" => "7kbG7Bk7S9Nt7ZSV", "Content-type" => "application/json"}).to_return body: @returned_body, status: 200
+      @result = OvhRestApi::ConsumerKey.generate :eu, "7kbG7Bk7S9Nt7ZSV"
+    end
+    it { expect(@result).to eq(JSON.parse @returned_body) }
+  end
+
+  it { expect(OvhRestApi::ConsumerKey.all_access_rules).to eq(
+    {"accessRules" => [
+      { "method" => "GET", "path" => "/*" },
+      { "method" => "POST", "path" => "/*" },
+      { "method" => "PUT", "path" => "/*" },
+      { "method" => "DELETE", "path" => "/*" }
+    ]}
+  )}
 end
