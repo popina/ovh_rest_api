81bbe21fa3ad02034aa18f53bd16644834c7a469
charlie <charlie.eissen@gmail.com>
2013-12-15 20:32:04 +0100

    error for request http

--- lib/ovh_rest_api/errors.rb
+++ lib/ovh_rest_api/errors.rb
@@ -4,4 +4,10 @@ module OvhRestApi
       super "only EU or CA are vailable for country code"
     end
   end
+  
+  class RequestException < Exception
+    def initialize
+      super "only EU or CA are vailable for country code"
+    end
+  end
 end
