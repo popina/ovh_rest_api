44a965b4f00fe76d0375cf77ee093e82a3e65532
Fonkymasto <fonkymasto@gmail.com>
2016-03-15 14:34:44 +0100

    Add Hosting Web

---
+++ lib/ovh_rest_api/hosting/web.rb
@@ -0,0 +1,25 @@
+module OvhRestApi
+  module Hosting
+    module Web
+      def hostings
+        get "/hosting/web"
+      end
+
+      def hosting service_name
+        get "/hosting/web/#{service_name}"
+      end
+
+      def subdomains service_name
+        get "/hosting/web/#{service_name}/attachedDomain"
+      end
+
+      def create_subdomain service_name, domain_name
+        post "/hosting/web/#{service_name}/attachedDomain", {domain: domain_name, path: domain_name}
+      end
+
+      def delete_domain service_name, domain_name
+        delete "/hosting/web/#{service_name}/attachedDomain/#{domain_name}"
+      end
+    end
+  end
+end
