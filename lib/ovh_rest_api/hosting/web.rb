bae6d29e8f44e3c10e67995e00e7212fe4c7de2f
Fonkymasto <fonkymasto@gmail.com>
2016-03-15 14:47:48 +0100

    Add Get Domain

--- lib/ovh_rest_api/hosting/web.rb
+++ lib/ovh_rest_api/hosting/web.rb
@@ -13,11 +13,15 @@ module OvhRestApi
         get "/hosting/web/#{service_name}/attachedDomain"
       end
 
+      def subdomain service_name, domain_name
+        get "/hosting/web/#{service_name}/attachedDomain/#{domain_name}"
+      end
+
       def create_subdomain service_name, domain_name
         post "/hosting/web/#{service_name}/attachedDomain", {domain: domain_name, path: domain_name}
       end
 
-      def delete_domain service_name, domain_name
+      def delete_subdomain service_name, domain_name
         delete "/hosting/web/#{service_name}/attachedDomain/#{domain_name}"
       end
     end
