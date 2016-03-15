301a2c0456ee116339aedc8cdcb8445957b4909d
Fonkymasto <fonkymasto@gmail.com>
2016-03-15 14:54:52 +0100

    Update create domain parameters

--- lib/ovh_rest_api/hosting/web.rb
+++ lib/ovh_rest_api/hosting/web.rb
@@ -17,8 +17,8 @@ module OvhRestApi
         get "/hosting/web/#{service_name}/attachedDomain/#{domain_name}"
       end
 
-      def create_subdomain service_name, domain_name
-        post "/hosting/web/#{service_name}/attachedDomain", {domain: domain_name, path: domain_name}
+      def create_subdomain service_name, path
+        post "/hosting/web/#{service_name}/attachedDomain", { domain: path+"."+domain_name, path: path, firewall: "none", ssl: false }
       end
 
       def delete_subdomain service_name, domain_name
